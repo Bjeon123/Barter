148867aa8842f6c223c4812c00b9336b6a4bb493
gamestar <rinayumiho@gmail.com>
2021-09-20 21:39:45 -0400

    Work on index for post

--- frontend/src/actions/post_actions.js
+++ frontend/src/actions/post_actions.js
@@ -1,6 +1,6 @@
 import * as PostApiUtil from '../util/post_api_util';
 
-export const RECEIVE_POST = "RECEIVE_Post";
+export const RECEIVE_POST = "RECEIVE_POST";
 export const RECEIVE_POSTS = "RECEIVE_POSTS";
 export const REMOVE_POST = "REMOVE_POST";
 export const RECEIVE_POST_ERRORS = "RECEIVE_POST_ERRORS";
