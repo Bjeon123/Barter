708516e26ba230868e262deb92d6d2bb4229e0b3
Jonathan Wong <jonathan.wong8@baruchmail.cuny.edu>
2021-09-21 21:42:08 -0400

    heroku stuff

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-
+const path = require('path');
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 const offers = require("./routes/api/offers");
@@ -15,6 +15,13 @@ mongoose
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!!"));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://dev:XYAGrFsebEUfgHMe@cluster0.0o2du.mongodb.net/test?retryWrites=true&w=majority',
-    secretOrKey: 'secret'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod')
+} else {
+    module.exports = require('./keys_dev')
 }
+
